5328e3e99bb087907985549038c62465e946f7be
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-08-05 18:25:35 +0900

    ログイン・ログアウトボタンを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 
-export const dynamic = 'force-static';
+//export const dynamic = 'force-static';
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,51 @@
-import TopCharacterSelect from '@/components/TopCharacterSelect';
-import clsx from 'clsx';
 
-export default function Home({
-  searchParams
-}: {
-  searchParams?: {
-    oshi?: string;
-  }
-}) {
-  const oshi = searchParams?.oshi || '';
+import { auth, signIn, signOut } from '@/auth';
+import Button from '@/components/Button';
+import { ArrowLeftEndOnRectangleIcon } from '@heroicons/react/24/outline';
+
+export default async function Home() {
+  const session = await auth();
   return (
-    <div>推しの組み合わせを分析します...</div>
+    <>
+      <div>推しの組み合わせを分析します...</div>
+      {session?.user?.name
+        ? <div>ログイン済みです!: {session.user.name}</div>
+        : <div>未ログインです</div>
+      }
+      {session &&
+        <form
+          action={async () => {
+            'use server'
+            await signOut();
+          }}
+          className='self-center'
+        >
+          <Button 
+            className='mt-2 p-2 flex flex-row'
+            type='submit'
+          >
+            ログアウト
+          </Button>
+        </form>
+      }
+      {!session &&
+        <form
+          action={async () => {
+            'use server'
+            await signIn('twitter');
+          }}
+          className='self-center'
+        >
+          <Button 
+            className='mt-2 p-2 flex flex-row'
+            type='submit'
+          >
+            X (Twitter) でログイン
+            <ArrowLeftEndOnRectangleIcon className='size-6 ml-2'/>
+          </Button>
+        </form>
+      }
+    </>
   );
 }
 
