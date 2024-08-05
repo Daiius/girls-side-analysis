2d3b7a1cb050ec369550e0fece39e8ac3661f6cf
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-08-05 10:11:03 +0900

    データベース分析用コマンドの実装

--- src/app/[charaName]/page.tsx
+++ src/app/[charaName]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
 
+import { getVotesRelatedToOshi } from '@/actions/votesActions';
+
 // 5分毎にアップデート
 // NOTE: 今はテスト用にちょっと頻繁にします
 export const revalidate = 30;
@@ -9,16 +11,26 @@ export async function generateStaticParams() {
   return [{ charaName: '氷上格' }, { charaName: '柊夜ノ介'}];
 }
 
-export default function Page({
+export default async function Page({
   params
 }: { params: { charaName: string }}) {
   const decodedCharaName = decodeURIComponent(params.charaName)
+
+  const data = await getVotesRelatedToOshi(decodedCharaName);
+
   return (
     <div className='flex flex-col items-center'>
       <div>{Date.now()}</div>
       <div>分析結果:</div>
       <div className='py-5'>
         {decodedCharaName} が好きな人は、○○も好きな人が多いです！
+      </div>
+      <div className='flex flex-col'>
+        {data.map(d =>
+          <div key={d.characterName}>
+            {d.characterName}: {d.count}票
+          </div>
+        )}
       </div>
       <div className={clsx(
         'w-64 h-32 outline outine-1',
