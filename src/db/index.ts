7fd1d2c7fa9fbdd9e82da81737426c371c0fc28d
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-08-19 05:41:27 +0900

    ホットリロード多様時のエラー対策を追加
    検証はまだ...

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,7 @@ export const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  idleTimeout: 10_000, 
 });
 
 
