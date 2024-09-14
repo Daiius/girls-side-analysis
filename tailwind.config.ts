8d6c84fba4f78721da8528ff2949de4a4d831f34
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-09-15 04:01:07 +0900

    アニメーション設定を追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,11 +16,17 @@ const config: Config = {
       },
       animation: {
         'vote-icon-rotation': 'vote-icon-keyframes 0.8s steps(1) infinite',
+        'logout-icon-scale': 'logout-icon-keyframes 0.8s steps(2) infinite',
       },
       keyframes: {
         'vote-icon-keyframes': {
           '0%' : { transform: 'translate(-50%,-50%) rotate(-15deg)' },
           '50%': { transform: 'translate(-50%,-50%) rotate(-30deg)' },
+        },
+        'logout-icon-keyframes': {
+          '0%' : { transform: 'translate(-10%,10%) rotate(0deg) scale(1.0)' },
+          '30%': { transform: 'translate(20%,-10%) rotate(10deg) scale(80%)' },
+          '70%': { transform: 'translate(40%,-20%) rotate(20deg) scale(60%)' },
         }
       }
     },
