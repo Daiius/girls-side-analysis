96c3c49a6dd16e37f62252bbec7d32e92e8eccc2
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-09-15 04:13:35 +0900

    コンポーネントサイズ指定を微調整

--- src/components/GSMessage.tsx
+++ src/components/GSMessage.tsx
@@ -24,7 +24,7 @@ const GSMessage: React.FC<
       'before:min-w-24 before:min-h-6 before:-top-6 before:-left-[2px]',
       'before:rounded-tl-lg',
       'before:[clip-path:polygon(0_0,_70%_0,_100%_100%,0_100%)]',
-      'h-[4.6rem]',
+      //'h-[4.6rem]',
       className,
     )}
     {...props}
@@ -32,7 +32,6 @@ const GSMessage: React.FC<
     <div className={clsx(
       'bg-white/90 rounded-lg px-4 py-1 flex flex-col',
       'shadow-inner',
-      heightFixed && 'h-[4.6rem]',
     )}>
       <div 
         className={clsx(
@@ -40,7 +39,7 @@ const GSMessage: React.FC<
           '[background-image:linear-gradient(0deg,_lightgray_1px,_transparent_1px)]',
           '[background-size:100%_1.5rem]',
           '[line-height:1.5rem]',
-          //'[background-position:0_0.2rem]', // 上部に余計な線が出る
+          heightFixed && 'h-[4.6rem]',
         )}
       >
         {children}
