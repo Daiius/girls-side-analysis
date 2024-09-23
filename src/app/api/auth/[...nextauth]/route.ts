c38877d287faf81320bf5297b2e1c7fb749d811a
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-09-23 16:37:09 +0900

    GET,POSTともにリバースプロキシ対応を追加

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,23 +4,26 @@ import { handlers } from '@/auth';
 
 import { NextRequest } from 'next/server';
 
-export async function GET(
-  req: NextRequest
-): Promise<Response> {
+const toForwardedRequest = (req: NextRequest): NextRequest => {
   const forwardedHost = req.headers.get('x-forwarded-host');
   const forwardedProto = req.headers.get('x-forwarded-proto');
   if (forwardedHost && forwardedProto) {
-    const forwardedUrl = `${forwardedProto}://${forwardedHost}/girls-side-analysis${req.nextUrl.pathname}?${req.nextUrl.searchParams.toString()}`;
+    const forwardedUrl = 
+      `${forwardedProto}://${forwardedHost}/girls-side-analysis${req.nextUrl.pathname}?${req.nextUrl.searchParams.toString()}`;
     const newReq = new NextRequest(forwardedUrl, {
       headers: req.headers,
       method: req.method,
       body: req.body,
     });
-    return await handlers.GET(newReq);
+    return newReq;
   } else {
-    return await handlers.GET(req);
+    return req;
   }
 }
 
-export const { POST } = handlers;
+export const GET: (req: NextRequest) => Promise<Response> = 
+  (req) => handlers.GET(toForwardedRequest(req));
+
+export const POST: (req: NextRequest) => Promise<Response> = 
+  (req) => handlers.POST(toForwardedRequest(req));
 
