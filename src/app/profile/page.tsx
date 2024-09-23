a8a6e3f61121ae041a4f3aec465b621bbe962b6b
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-09-23 16:39:42 +0900

    エラー処理にConfigurationを追加

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,12 +8,15 @@ export default async function Page({
 }: { 
   searchParams?: { error?: string } 
 }) {
+  console.log('### /profile page access!! ###');
   const session = await auth();
   const errorMessage =
     searchParams?.error === 'OAuthCallbackError'
       ? 'X(Twitter)認証がキャンセルされました' :
     searchParams?.error === 'UnknownAction'
       ? 'アクションをパース出来ませんでした' :
+    searchParams?.error === 'Configuration' 
+      ? 'タイムアウト、または設定エラーです' :
     searchParams?.error
       ? `不明なエラー: ${searchParams?.error}` :
     undefined;
