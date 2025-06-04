69b86c99c9533c304933515d826ceb1aec099812
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-06-05 04:46:06 +0900

    ユーザ情報取得APIを追加

--- server/src/dto.rs
+++ server/src/dto.rs
@@ -1,7 +1,10 @@
 
 use serde::Serialize;
 use utoipa::ToSchema;
-use crate::entity::characters;
+use crate::entity::{
+    characters,
+    user_states,
+};
 
 #[derive(Serialize, ToSchema)]
 pub struct CharacterDto {
@@ -16,6 +19,25 @@ impl From<characters::Model> for CharacterDto {
             name: model.name,
             series: model.series,
             sort: model.sort,
+        }
+    }
+}
+
+#[derive(Serialize, ToSchema)]
+pub struct UserStateDto {
+    twitter_id: String,
+    recorded_time: String,
+    series: u8,
+    status: String,
+}
+
+impl From<user_states::Model> for UserStateDto {
+    fn from(model: user_states::Model) -> Self {
+        Self {
+            twitter_id: model.twitter_id,
+            recorded_time: model.recorded_time.to_string(),
+            series: model.series,
+            status: model.status,
         }
     }
 }
