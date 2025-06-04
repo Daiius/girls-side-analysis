fe799ff9868827f8b7bbe2c216aee8a8fd9a587b
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-06-04 22:50:19 +0900

    バックエンドプロジェクトを作成

---
+++ server/src/main.rs
@@ -0,0 +1,19 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, axum!" }))
+        .route("/characters", get(get_characters));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap()
+}
+
+async fn get_characters() {
+}
