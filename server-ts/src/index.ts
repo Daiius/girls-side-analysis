5d059110efc9a1b085b1ef00c2d4f512e921f789
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-07-31 16:12:10 +0900

    サーバ側の動作確認と修正
    - zValidatorのparameter指定方法を修正
    - Bearerトークンの構文を修正
    - 投票後に変更があったキャラ名を返却する機能を追加
      (revalidatePathでピンポイントに更新する用)

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -26,7 +26,7 @@ const app = new Hono()
 app.use('*', async (c, next) => {
   const authHeader = c.req.header('Authorization')
   if (!authHeader) return c.body(null, 401)
-  const tokens = authHeader.split(': ')
+  const tokens = authHeader.split(' ')
   if (tokens.length != 2) return c.body(null, 400)
   const [bearer, apiKeyToken] = tokens
   if (bearer !== 'Bearer') return c.body(null, 400)
@@ -44,11 +44,20 @@ const route = app
   )
   .get(
     '/analysis/:charaName', // 特定のキャラクターに関連する投票データを取得します
-    zValidator('param', z.string()),
+    zValidator('param', z.object({ charaName: z.string() })),
     async c => {
-      const charaName = c.req.valid('param')
-      const data = await getLatestVotesForAnalysis(charaName)
-      return c.json(data, 200)
+      const { charaName } = c.req.valid('param')
+      try {
+        const data = await getLatestVotesForAnalysis(charaName)
+        return c.json(data, 200)
+      } catch (e) {
+        if (e instanceof Error) {
+          console.error(`exception at GET /analysis/${charaName}: ${e.message}`)
+        } else {
+          console.error(`unknown error at GET /analysis/${charaName}`)
+        }
+        return c.body(null, 500)
+      }
     }
   )
   .get(
@@ -60,18 +69,18 @@ const route = app
   )
   .get(
     '/timeline/:charaName', // 特定のキャラについて、関連するキャラの時系列データを取得します
-    zValidator('param', z.string()),
+    zValidator('param', z.object({ charaName: z.string() })),
     async c => {
-      const charaName = c.req.valid('param')
+      const { charaName } = c.req.valid('param')
       const data = await getTimelineData(charaName)
       return c.json(data, 200)
     },
   )
   .get(
     '/users/:id', // 特定のユーザの最新の情報を取得します
-    zValidator('param', z.string()),
+    zValidator('param', z.object({ id: z.string() })),
     async c => {
-      const id = c.req.valid('param')
+      const { id } = c.req.valid('param')
       const data = await getLatestUserState(id)
       return c.json(data, 200)
     },
@@ -85,7 +94,7 @@ const route = app
   )
   .post(
     '/users/:id', // ユーザ状態に最新状態からの変更があれば更新します
-    zValidator('param', z.string()),
+    zValidator('param', z.object({ id: z.string() })),
     zValidator('json',
       z.array(z.object({
         series: z.number(),
@@ -93,7 +102,7 @@ const route = app
       })),
     ),
     async c => {
-      const id = c.req.valid('param')
+      const { id } = c.req.valid('param')
       const data = c.req.valid('json')
       try {
         await insertUserStatesIfUpdated({
@@ -113,25 +122,29 @@ const route = app
   )
   .get(
     '/votes/:id', // 特定のユーザの最新の投票結果を取得します
-    zValidator('param', z.string()),
+    zValidator('param', z.object({ id: z.string() })),
     async c => {
-      const id = c.req.valid('param')
+      const { id } = c.req.valid('param')
       const data = await getLatestVotes(id)
       return c.json(data, 200)
     },
   )
+  // ユーザの投票を受け付け、前回と変更があれば反映します
+  // Next.js側で効率良くrevalidatePathを行うため、前回の投票と今回の投票の
+  // 差分に当たるキャラをクライアントに返します
   .post(
-    '/votes/:id', // ユーザの投票を受け付け、前回と変更があれば反映します
-    zValidator('param', z.string()),
+    '/votes/:id', 
+    zValidator('param', z.object({ id: z.string() })),
     zValidator('json', z.array(z.object({
       characterName: z.string(),
       level: z.number(),
     }))),
     async c => {
-      const id = c.req.valid('param')
+      const { id } = c.req.valid('param')
       const votes = c.req.valid('json')
       try {
-        await insertVotesIfUpdated({ twitterID: id, data: votes })
+        const updated = await insertVotesIfUpdated({ twitterID: id, data: votes })
+        return c.json(updated, 200)
       } catch (e) {
         if (e instanceof Error) {
           console.error(`exception at POST /votes/${id}: ${e.message}`)
--- server-ts/src/lib/characters.ts
+++ server-ts/src/lib/characters.ts
@@ -1,8 +1,14 @@
 
 import { db } from '../db'
+import {
+  characters
+} from '../db/schema'
 
 /**
  * キャラデータの一覧を取得します
  */
-export const getCharacters = async () => await db.query.characters.findMany()
+export const getCharacters = async () => 
+  await db.query.characters.findMany({
+    orderBy: [characters.series, characters.sort]
+  })
 
