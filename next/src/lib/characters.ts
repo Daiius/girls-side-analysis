93ebafdf2c029c717fabc22ec3878f5e6ec929f2
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-08-06 00:03:23 +0900

    極端な回数呼ばれかねないAPIにキャッシュを設定

--- next/src/lib/characters.ts
+++ next/src/lib/characters.ts
@@ -2,9 +2,10 @@ import { client } from './apiClient'
 
 /**
  * キャラデータの一覧を取得します
+ * このAPIの応答は滅多に変わらないはずなので、1日キャッシュします
  */
 export const getCharacters = async () => {
-  const res = await client.characters.$get()
+  const res = await client({ revalidate: 86400 }).characters.$get()
   if (res.ok) {
     return await res.json()
   }
