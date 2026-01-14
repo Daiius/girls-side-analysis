ffc8c4deda177242d02f3464ce227db3a2730e7d
Daiji Yamashita <yamashita.daiji@gmail.com>
2026-01-14 23:58:43 +0900

    update next & production container nodejs & drizzle-orm

--- server-ts/src/lib/characters.ts
+++ server-ts/src/lib/characters.ts
@@ -1,14 +1,12 @@
 
 import { db } from '../db'
-import {
-  characters
-} from '../db/schema'
 
 /**
  * キャラデータの一覧を取得します
  */
 export const getCharacters = async () => 
   await db.query.characters.findMany({
-    orderBy: [characters.series, characters.sort]
+    //orderBy: [characters.series, characters.sort]
+    orderBy: { series: 'asc', sort: 'asc' }
   })
 
